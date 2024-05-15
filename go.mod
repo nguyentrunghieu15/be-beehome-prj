681e198b3a06152c8744b31867a824b22bc093d3
nguyentrunghieugl15 <nguyentrunghieugl15112002@gmail.com>
2024-05-15 15:08:30 +0700

    FEAT: Add mail

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.23.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.10
 )
 
@@ -26,5 +28,5 @@ require (
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
-	gorm.io/driver/postgres v1.5.7 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -1,1 +1,53 @@
 package mail
+
+import (
+	"os"
+	"strconv"
+
+	"gopkg.in/gomail.v2"
+)
+
+type ContentType string
+
+const (
+	PLAIN_TEXT ContentType = "text/plain"
+	HTML       ContentType = "text/html"
+)
+
+type Letter struct {
+	From    string
+	To      []string
+	Cc      []string
+	Subject string
+	Type    ContentType
+	Body    string
+}
+
+type AuthStmp struct {
+	Email    string
+	Password string
+}
+
+func SendMail(auth AuthStmp, letter Letter) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", letter.From)
+	m.SetHeader("To", letter.To...)
+	m.SetHeader("Cc", letter.Cc...)
+	m.SetHeader("Subject", letter.Subject)
+	m.SetBody(string(letter.Type), letter.Body)
+
+	host := os.Getenv("MAIL_HOST")
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+
+	if err != nil {
+		return err
+	}
+
+	d := gomail.NewDialer(host, port, auth.Email, auth.Password)
+
+	// Send the email
+	if err := d.DialAndSend(m); err != nil {
+		return err
+	}
+	return nil
+}
