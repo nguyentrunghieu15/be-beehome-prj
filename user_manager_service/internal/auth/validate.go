6d9b8f5f09a57acd5c008473d753c6c7e52cee59
Hiro <nguyentrunghieugl15112002@gmail.com>
2024-05-20 06:20:25 +0700

    Add validate for auth api

--- user_manager_service/internal/auth/validate.go
+++ user_manager_service/internal/auth/validate.go
@@ -7,6 +7,10 @@ import (
 
 func SetRules(v validator.IValidator) {
 	SetRulesOfLoginRequest(v)
+	SetRulesOfForgetPasswordRequest(v)
+	SetRulesOfRefreshTokenRequest(v)
+	SetRulesOfResetPasswordRequest(v)
+	SetRulesOfSignUpRequest(v)
 }
 
 func SetRulesOfLoginRequest(v validator.IValidator) {
@@ -15,4 +19,38 @@ func SetRulesOfLoginRequest(v validator.IValidator) {
 		"Password": "required,min=8",
 	}
 	v.RegisterRules(validationRules, &authapi.LoginRequest{})
+}
+
+func SetRulesOfRefreshTokenRequest(v validator.IValidator) {
+	validationRules := map[string]string{
+		"RefreshToken": "required",
+	}
+	v.RegisterRules(validationRules, &authapi.RefreshTokenRequest{})
+}
+
+func SetRulesOfForgetPasswordRequest(v validator.IValidator) {
+	validationRules := map[string]string{
+		"Email": "required,email",
+	}
+	v.RegisterRules(validationRules, &authapi.ForgotPasswordRequest{})
+}
+
+func SetRulesOfResetPasswordRequest(v validator.IValidator) {
+	validationRules := map[string]string{
+		"NewPassword":     "required,min=8",
+		"ConfirmPassword": "required,min=8",
+		"ResetToken":      "required",
+	}
+	v.RegisterRules(validationRules, &authapi.ResetPasswordRequest{})
+}
+
+func SetRulesOfSignUpRequest(v validator.IValidator) {
+	validationRules := map[string]string{
+		"Email":     "required,email",
+		"Password":  "required,min=8",
+		"Phone":     "required,min=9",
+		"FirstName": "required",
+		"LastName":  "required",
+	}
+	v.RegisterRules(validationRules, &authapi.SignUpRequest{})
 }
