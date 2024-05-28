858f0c9b21f4238f88efdee8b1dfdfdfcb9f916a
nguyentrunghieugl15 <nguyentrunghieugl15112002@gmail.com>
2024-05-28 18:42:42 +0700

    FEAT: Aadd migration pro

--- api/build.sh
+++ api/build.sh
@@ -17,3 +17,11 @@ protoc -I . \
 protoc -I . \
     --go_out . --go_opt paths=source_relative \
     ./api/pro-api/message.proto
+
+protoc -I . \
+    --go_out . --go_opt paths=source_relative \
+    --go-grpc_out . --go-grpc_opt paths=source_relative \
+    --grpc-gateway_out . --grpc-gateway_opt paths=source_relative \
+    --openapiv2_out . \
+    --openapiv2_opt use_go_templates=true \
+    ./api/pro-api/pro-api.proto
--- pro-manager-service/internal/datasource/migration/migration.go
+++ pro-manager-service/internal/datasource/migration/migration.go
@@ -1,8 +1,19 @@
 package migration
 
-import "github.com/nguyentrunghieu15/be-beehome-prj/internal/database"
+import (
+	"github.com/nguyentrunghieu15/be-beehome-prj/internal/database"
+	"github.com/nguyentrunghieu15/be-beehome-prj/pro-manager-service/internal/datasource"
+)
 
 func MigrationDatasource(db *database.PostgreDb) error {
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-	return db.AutoMigrate()
+	return db.AutoMigrate(
+		&datasource.PostalCode{},
+		&datasource.GroupService{},
+		&datasource.Service{},
+		&datasource.PaymentMethod{},
+		&datasource.Provider{},
+		&datasource.Review{},
+		&datasource.SocialMedia{},
+	)
 }
