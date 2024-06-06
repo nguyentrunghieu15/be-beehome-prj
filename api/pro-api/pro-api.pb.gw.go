0a394d8e4eb8f8dc757d1d0ff87021b165eefbf3
nguyentrunghieugl15 <nguyentrunghieugl15112002@gmail.com>
2024-06-06 18:55:30 +0700

    FIX: API find pros

--- api/pro-api/pro-api.pb.gw.go
+++ api/pro-api/pro-api.pb.gw.go
@@ -32,9 +32,20 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+var (
+	filter_ProService_FindPros_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_ProService_FindPros_0(ctx context.Context, marshaler runtime.Marshaler, client ProServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq FindProsRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ProService_FindPros_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.FindPros(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -44,6 +55,13 @@ func request_ProService_FindPros_0(ctx context.Context, marshaler runtime.Marsha
 func local_request_ProService_FindPros_0(ctx context.Context, marshaler runtime.Marshaler, server ProServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq FindProsRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ProService_FindPros_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.FindPros(ctx, &protoReq)
 	return msg, metadata, err
