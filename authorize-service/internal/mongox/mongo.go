ab36604250ee4fa00b2c75b10a98cbe6a01c5c28
nguyentrunghieu15 <78260236+nguyentrunghieu15@users.noreply.github.com>
2024-07-05 17:58:30 +0700

    fix mongox

--- authorize-service/internal/mongox/mongo.go
+++ authorize-service/internal/mongox/mongo.go
@@ -13,7 +13,7 @@ type MomgoClientConfig struct {
 	Address string
 	Uri string
 	Timeout time.Duration
-}filter := bson.D{{"name", "Bagels N Buns"}}
+}
 
 type ClientWrapper struct {
 	config *MomgoClientConfig
@@ -22,12 +22,29 @@ type ClientWrapper struct {
 	once sync.Once
 }
   
-func (istn *ClientWrapper) connect(){
+func (istn *ClientWrapper) connect() error {
+	ctx, cancel := context.WithTimeout(context.Background(), istn.config.Timeout)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%v:%v@%v",istn.config.UserName,istn.config.Password,istn.config.Address)))
+	if err != nil { return err }
+	istn.client = client
+	return nil
+}
 
+func (istn *ClientWrapper) doConnect() {
+	for {
+		err:= istn.connect()
+		if err != nil {
+			time.Sleep(300)
+			continue
+		}
+		break
+	}
 }
 
 func (istn *ClientWrapper) Client()  (*mongo.Client,error) {
-
+	istn.once.Do(istn.doConnect)
+	return istn.client ,nil
 }
 
 func (istn *ClientWrapper) Db()  (*mongo.Database,error) {
@@ -35,22 +52,17 @@ func (istn *ClientWrapper) Db()  (*mongo.Database,error) {
 }
 
 type Repository[T any] struct{
-	Client ClientWrapper
+	Client *ClientWrapper
 	Collection string
 }
 
 
 func (istn *Repository[T]) FindOneByAtribute(name string, value interface{}) (*T,error) {
 	filter := bson.D{{name, value}}
-
 	result  = new(T)
 	err = istn.Client.Db().Collection(istn.Collection).(context.TODO(), filter).Decode(&result)
-	// Prints a message if no documents are matched or if any
-	// other errors occur during the operation
 	if err != nil {
 		return nil, err
 	}
 	return result ,nil
 }
-
-const DefaultClientWrapper := new(ClientWrapper)
